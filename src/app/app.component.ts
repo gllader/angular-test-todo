040198674e9fc81eb6f01ebe464bc92e39375886
gll <guilherme.lopes123@hotmail.com>
2021-05-07 14:54:28 -0300

    layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo';
+  todoArray=[]
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+  deleteItem(index){
+    this.todoArray.splice(index, 1)
+    console.log("delete item")
+  }
 }
